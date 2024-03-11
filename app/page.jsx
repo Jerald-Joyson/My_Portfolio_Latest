cdebee2424a80ce123cd44852623c7cff8cee18c
Jerald-Joyson <jeraldjoyson.inc@gmail.com>
2024-03-11 22:20:42 +0530

    direction to contact page is set

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import About from "@/components/about";
+import Cta from "@/components/cta";
 import Hero from "@/components/hero";
 import Reviews from "@/components/reviews";
 import Services from "@/components/services";
@@ -12,6 +13,7 @@ export default function Home() {
       <Services />
       <Work />
       <Reviews />
+      <Cta />
     </main>
   );
 }
